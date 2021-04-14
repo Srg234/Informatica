a74ba302c5f2523ec32d295884b842ebe6ef8aa1
sergio <s2rg342g5t32rr2zg@gmail.com>
2021-04-13 19:27:36 -0500

    canvas

--- Clases/Javascript/trabajo clase/Canvas y dibujos/Lápizyteléfono.js
+++ Clases/Javascript/trabajo clase/Canvas y dibujos/Lápizyteléfono.js
@@ -12,9 +12,13 @@ ctx.strokeRect(50,0,100, 45)
 ctx.strokeRect(50,15,100, 15)
 // debo iniciar un camino
 ctx.beginPath()
-ctx.fillArc= "red"
+ctx.fillArc = "red"
 ctx.arc(150, 23, 23, Math.PI*1.5, 2.5* Math.PI);
 ctx.fill()
+
+ctx.lineWidth= 1
+ctx.strokeStyle = "black"
+ctx.beginPath()
 
 ctx.moveTo(0, 23)
 ctx.lineTo(50, 0)
@@ -26,11 +30,13 @@ ctx.moveTo(0, 23)
 ctx.lineTo(20, 30)
 ctx.lineTo(20, 15)
 ctx.stroke()
-ctx.stroke()
+
 ctx.fillStyle = "black"
 ctx.fill()
 ctx.moveTo(0,50)
 
+
+ctx.beginPath()
 ctx.fillStyle = "black"
 ctx.fillRect(0,65,100, 45)
 ctx.fillRect(100,65,25, 15)
@@ -49,6 +55,10 @@ ctx.fillRect(39,83,9, 9)
 ctx.fillRect(39,95,9, 9)
 ctx.fillStyle = "blue"
 ctx.fillRect(54,71,39, 33)
+
+
+
+
 ctx.endPath()
 
 // ctx.endPath()
