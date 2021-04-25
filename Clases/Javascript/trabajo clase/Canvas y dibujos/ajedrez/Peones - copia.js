ae113dcfb4f6cd9f2fffbff761049b81f4e196aa
sergio <s2rg342g5t32rr2zg@gmail.com>
2021-04-25 11:11:40 -0500

    Peones con tamaño y color definidos

--- Clases/Javascript/trabajo clase/Canvas y dibujos/ajedrez/Peones - copia.js
+++ Clases/Javascript/trabajo clase/Canvas y dibujos/ajedrez/Peones - copia.js
@@ -18,7 +18,7 @@ SecondSquareY=SecondSquareY+90*SizeSelect}
 while(FirstSquareY<70*5*SizeSelect)}
 //si el tamaño es mayor a 3 no se centra bien en el tablero por lo que no se dibuja el tablero(Si logro descubrir cómo lo arreglaré más tarde)
 //cambie SizeSelect para cambiar el tamaño de los peones
-let SizeSelect= 4
+let SizeSelect= 1
 
 let Xcambiante=25
 
@@ -29,7 +29,7 @@ Xcambiante=Xcambiante+90*SizeSelect}
 while(Xcambiante<70*5*SizeSelect)}
 
 
-function DrawPawn(xCentral,yCentral,SizeSelect){
+function DrawPawn(xCentral,yCentral,SizeSelect,ColorSelect){
 ctx.beginPath()
 ctx.lineWidth= 1
 
@@ -54,21 +54,20 @@ ctx.lineTo((xCentral*SizeSelect)+6*SizeSelect,49*SizeSelect+(yCentral*SizeSelect
 ctx.lineTo((xCentral*SizeSelect)+3*SizeSelect,43*SizeSelect+(yCentral*SizeSelect))
 ctx.lineTo((xCentral*SizeSelect)+11*SizeSelect,42*SizeSelect+(yCentral*SizeSelect))
 ctx.lineTo((xCentral*SizeSelect)+3*SizeSelect,41*SizeSelect+(yCentral*SizeSelect))
-
+ctx.fillStyle=ColorSelect
+    ctx.fill()
 ctx.stroke()}
 let xBase=45-((SizeSelect-1)*8)
 for(let xBase=45-((SizeSelect-1)*8); xBase < (8*45)+1; xBase= xBase+45){
 
-    DrawPawn(xBase,45-((SizeSelect-1)*8),SizeSelect)
-    ctx.fillStyle = "#F9E4B7"
-    ctx.fill()
+    DrawPawn(xBase,45-((SizeSelect-1)*8),SizeSelect,"#F9E4B7")
+    
     }
     
 
 for(let xBase2=45-((SizeSelect-1)*8); xBase2< (8*45)+1; xBase2= xBase2+45){
-    DrawPawn(xBase2,270-((SizeSelect-1)*8),SizeSelect)
-    ctx.fillStyle="black"
-    ctx.fill()
+    DrawPawn(xBase2,270-((SizeSelect-1)*8),SizeSelect,"black")
+    
     ctx.stroke()
     }
 
